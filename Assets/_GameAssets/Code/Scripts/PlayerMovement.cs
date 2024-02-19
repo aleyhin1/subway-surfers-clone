b5f5e6b0b81e346b6c11cda92f8e35652b8f6443
Batu <aleyhin1@gmail.com>
2024-02-19 08:10:05 +0300

    movement polished and refactored

--- Assets/_GameAssets/Code/Scripts/PlayerMovement.cs
+++ Assets/_GameAssets/Code/Scripts/PlayerMovement.cs
@@ -1,37 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Build.Reporting;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField, Range(0, 100)] private float _movementDistance;
-    private float _leftPositionX;
-    private float _rightPositionX;
 
-    private void Start()
-    {
-        _leftPositionX = -_movementDistance;
-        _rightPositionX = _movementDistance;
-    }
+    [SerializeField, Range(0, 100)] private float _movementDistance;
+    [SerializeField] private AnimationCurve _movementCurve;
+    [SerializeField] private float _movementSpeed;
+    private bool _isMoving;
+
+    
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow) && transform.position.x > _leftPositionX)
+        if (!_isMoving)
         {
-            Move(_leftPositionX);
-        }
-        else if (Input.GetKeyDown(KeyCode.RightArrow) && transform.position.x < _rightPositionX)
-        {
-            Move(_rightPositionX);
+            if (Input.GetKeyDown(KeyCode.LeftArrow) && transform.position.x > -_movementDistance)
+            {
+                StartCoroutine(Move(-_movementDistance));
+            }
+            else if (Input.GetKeyDown(KeyCode.RightArrow) && transform.position.x < _movementDistance)
+            {
+                StartCoroutine(Move(_movementDistance));
+            }
         }
     }
 
-    private void Move(float distance)
+    private IEnumerator Move(float distance)
     {
-        float positionToMoveX = transform.position.x + distance;
-        Vector3 positionToMove = new Vector3(positionToMoveX, 0, 0);
+        _isMoving = true;
+        Vector3 initialPosition = transform.position;
+        Vector3 positionToMove = new Vector3(initialPosition.x + distance, initialPosition.y, initialPosition.z);
 
-        transform.position = positionToMove;
+        while (Mathf.Abs(transform.position.x - positionToMove.x) > .0001f)
+        {
+            float distanceMoved = Mathf.Abs(transform.position.x - initialPosition.x);
+            float step = _movementCurve.Evaluate(distanceMoved);
+            transform.position = Vector3.MoveTowards(transform.position, positionToMove, step * Time.deltaTime * _movementSpeed);
+
+            yield return new WaitForEndOfFrame();
+        }
+        _isMoving = false;
     }
 }
